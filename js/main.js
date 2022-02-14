89df777eed7bc7b6caf9a389d1c0308227ae29a2
BenderMaster <franciscodeassissouza21@gmail.com>
2022-02-14 13:05:00 -0300

    Adicionar pastas js

---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+function clicou(){
+    document.getElementById("agradeco").innerHTML = "Obrigado por clicar!";
+}
+
+function redirecionar(){
+    window.open("http://www.google.com");
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Pegou ;-;";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Você não me pega!";
+}
+
+/*
+var validar;
+
+function validaIdade(idade){
+    if(idade >= 18) {
+        validar = true;
+    }else{
+        validar = false;
+    } 
+    return validar;
+}
+
+var idade = prompt("qual sua idade");
+validaIdade(idade);
+console.log(validar);
+*/
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 15));
+*/
+
+/*
+var d = new Date();
+alert(d.getFullYear());
+*/
+
+/*var count
+for(count=0; count <= 5; count++){
+    console.log(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+};
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("Maior de idade");
+}else {
+    alert("Menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "verde"}]
+console.log(frutas[1].nome);
+console.log(frutas);
+*/
+
+/*
+var lista = ["maça", "morango", "laranja"]
+lista.pop();
+lista.push("uva");
+console.log(lista.toString());
+console.log(lista.length);
+console.log(lista.join(" | "));
+*/
+
+/*var nome = "Bender Master";
+var idade = 27;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+*/
+
+//alert(idade + idade2);
+//alert(nome + " tem " + idade + " anos.");
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toUpperCase());
